54158632f250bc0027bd10f1512d679fe7eaa44c
freetimezp <kraftukrnet@ukr.net>
2022-09-29 18:47:50 +0300

    08. js-eatme-01. Intro component as video block.

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 
+import {meal} from '../../constants';
+import {BsFillPlayFill, BsFillPauseFill} from "react-icons/bs";
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+    const [playVideo, setPlayVideo] = useState(false);
+    const vidRef = useRef();
+
+    const handleVideo = () => {
+        setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+        if(playVideo) {
+            vidRef.current.pause();
+        }else{
+            vidRef.current.play();
+        }
+    }
+
+    return (
+        <div className="app__video">
+            <video
+                src={meal} type="video/mp4" loop controls={false} muted
+                ref={vidRef}
+            />
+
+            <div className="app__video-overlay flex__center">
+                <div className="app__video-overlay_circle flex__center" onClick={handleVideo}>
+                    {playVideo ? (
+                        <BsFillPauseFill color="#fff" fontSize={30} />
+                    ) : (
+                        <BsFillPlayFill color="#fff" fontSize={30} />
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Intro;
