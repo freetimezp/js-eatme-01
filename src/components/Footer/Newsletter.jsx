f8585d107c49e7eec3427072771672edddc9379b
freetimezp <kraftukrnet@ukr.net>
2022-09-30 19:23:06 +0300

    14. js-eatme-01. Sendmail fix. 03.

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,36 +1,48 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 
 import SubHeading from  '../SubHeading/SubHeading';
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 
 import './Newsletter.css';
 
 const Newsletter = () => {
-    const contactForm = document.getElementById('contact-form');
-    const contactEmail = document.getElementById('contact-email');
-    const contactMessage = document.getElementById('contact-message');
+    const [userEmail, setUserEmail] = useState('');
+
+    const refEmail = useRef();
+    const refMessage = useRef();
+
+    useEffect(() => {
+        const elEmail = refEmail.current;
+        const elMessage = refMessage.current;
+    }, []);
 
     const sendMail = (e) => {
         e.preventDefault();
-        e.stopPropagation();
-        e.nativeEvent.stopImmediatePropagation();
 
-        if(contactEmail.value === '') {
-            contactMessage.classList.remove('color-green');
-            contactMessage.classList.add('color-red');
+        //console.log(refEmail.current);
 
-            contactMessage.textContent = 'Please write your email..';
+        if(refEmail.current.value === '') {
+            //console.log('error');
+            refMessage.current.className = 'color-red';
+            refMessage.current.textContent = 'Please write your email..';
         }else{
-            emailjs.sendForm('service_lqpyxd1', 'template_3ty9whb', '#contact-form', 'kk5oM2vPMYsBD367x');
+            emailjs.sendForm('service_lqpyxd1', 'template_3ty9whb', '#contact-form', 'kk5oM2vPMYsBD367x')
+                .then((response) => {
+                    console.log('SUCCESS!', response.status, response.text);
+                }, (err) => {
+                    console.log('FAILED...', err);
+                });
 
-            contactMessage.classList.remove('color-red');
-            contactMessage.classList.add('color-green');
+            //console.log(refEmail.current);
+            refMessage.current.className = 'color-green';
+            refMessage.current.textContent = 'Hello! New Subscriber!';
 
-            contactMessage.textContent = 'Hello! New Subscriber!';
+            setTimeout(() => {
+                setUserEmail('');
+                refMessage.current.textContent = '';
+            }, 5000)
         }
     }
-
-    contactForm?.addEventListener('submit', sendMail);
 
     return (
         <div className="app__newsletter">
@@ -39,9 +51,13 @@ const Newsletter = () => {
                 <h1 className="headtext__cormorant">Subscribe to Our Newsletter</h1>
                 <p className="p__opensans">And never miss latest Updates!</p>
             </div>
-            <form className="app__newsletter-input flex__center" id="contact-form">
-                <input type="email" placeholder="Enter your email" name="user_email" id="contact-email" />
-                <p className="contact-message" id="contact-message"></p>
+            <form className="app__newsletter-input flex__center" id="contact-form" onSubmit={sendMail}>
+                <input
+                    type="email" placeholder="Enter your email" name="user_email" id="contact-email"
+                    onChange={(e) => setUserEmail(e.target.value)}
+                    ref={refEmail} value={userEmail}
+                />
+                <p id="contact-message" ref={refMessage}></p>
                 <button className="custom__button">
                     Subscribe
                 </button>
